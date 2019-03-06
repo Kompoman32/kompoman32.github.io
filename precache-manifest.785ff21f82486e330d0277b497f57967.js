fb32e8d3ee45fb52c63e26b242090bb41c90c1f3
kompoman32 <kompoman32@yandex.ru>
2019-03-06 14:12:54 +0500

    .

---
+++ precache-manifest.785ff21f82486e330d0277b497f57967.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "9b561ef12512ca76dd6b",
+    "url": "/static/js/main.9b561ef1.chunk.js"
+  },
+  {
+    "revision": "a539ea9d3b1acab9c730",
+    "url": "/static/js/2.a539ea9d.chunk.js"
+  },
+  {
+    "revision": "9b561ef12512ca76dd6b",
+    "url": "/static/css/main.21c86543.chunk.css"
+  },
+  {
+    "revision": "90bc6997e9533282b7ee4a5537ac0f50",
+    "url": "/index.html"
+  }
+];
